6c66830a8de97555f45954c2c7e34b5c7ee58114
Paras Dua <paras.dua@mobcoder.com>
2022-10-07 13:43:35 +0530

    fixed checkbox issue

--- src/components/FoodList.tsx
+++ src/components/FoodList.tsx
@@ -78,22 +78,22 @@ export function FoodList() {
       <Container className="flex flex-row justify-center items-center bg-[#EDECFA] mb-4">
         <h2 className="italic font-bold text-xl py-12">List of Food Items</h2>
       </Container>
-      <Container className="flex md:flex-row justify-center items-center mb-4 sm:flex-col">
+      <Container className="flex lg:flex-row justify-center items-center mb-4 sm:flex-col">
 
         {searchText === "" ? (
           <div
-            // style={{
-            //   flexDirection: "row",
-            //   display: "flex",
-            //   justifyContent: "center",
-            // }}
+            style={{
+              flexDirection: "row",
+              display: "flex",
+              justifyContent: "center",
+            }}
           >
             <div
-              // style={{
-              //   flexDirection: "row",
-              //   display: "flex",
-              //   alignItems: "center",
-              // }}
+              style={{
+                flexDirection: "row",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
               <Typography>Carbohydrates</Typography>
               <Checkbox
@@ -102,11 +102,11 @@ export function FoodList() {
               />
             </div>
             <Box
-              // sx={{
-              //   flexDirection: "row",
-              //   display: "flex",
-              //   alignItems: "center",
-              // }}
+              sx={{
+                flexDirection: "row",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
               <Typography>Protien</Typography>
               <Checkbox
@@ -115,11 +115,11 @@ export function FoodList() {
               />
             </Box>
             <Box
-              // sx={{
-              //   flexDirection: "row",
-              //   display: "flex",
-              //   alignItems: "center",
-              // }}
+              sx={{
+                flexDirection: "row",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
               <Typography>Minerals</Typography>
               <Checkbox
@@ -128,11 +128,11 @@ export function FoodList() {
               />
             </Box>
             <Box
-              // sx={{
-              //   flexDirection: "row",
-              //   display: "flex",
-              //   alignItems: "center",
-              // }}
+              sx={{
+                flexDirection: "row",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
               <Typography>Fats</Typography>
               <Checkbox
@@ -141,11 +141,11 @@ export function FoodList() {
               />
             </Box>
             <Box
-              // sx={{
-              //   flexDirection: "row",
-              //   display: "flex",
-              //   alignItems: "center",
-              // }}
+              sx={{
+                flexDirection: "row",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
               <Typography>Vitamins</Typography>
               <Checkbox
@@ -154,11 +154,11 @@ export function FoodList() {
               />
             </Box>
             <Box
-              // sx={{
-              //   flexDirection: "row",
-              //   display: "flex",
-              //   alignItems: "center",
-              // }}
+              sx={{
+                flexDirection: "row",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
               <Typography>Dietary Fibre</Typography>
               <Checkbox
